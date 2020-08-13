2a589b92988431b13e820100bab65201fdad8d09
fdj <461960962@qq.com>
2020-08-13 17:02:30 +0800

    add  miniprogram release function

--- extend/ky/MiniPlatform/Request/WxaRelease.php
+++ extend/ky/MiniPlatform/Request/WxaRelease.php
@@ -13,7 +13,7 @@ class WxaRelease
 {
     private $url = "https://api.weixin.qq.com/wxa/release";
     private $getParams = array();
-    private $postParams = array();
+    private $postParams = array('a' => 1);
 
     /**
      * 获取请求url
